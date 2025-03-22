23e1aa8d0c692ccff5769e71782d736ce7840d70
Ivan Santos <ivansantos.ronem@gmail.com>
2025-03-22 14:11:03 -0300

    feat: create installer (gcc/Java/Docker)

---
+++ installer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Update Development Machine"
+sleep 4
+apt update -y && apt upgrade -y curl
+sleep 3
+
+echo "Update Successfully"
+
+sleep 5
+
+echo "OS Release"
+curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/release.sh | bash
+
+sleep 5
+
+echo "gcc Installer"
+curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/gcc.sh | bash
+sleep 3
+
+echo "Java Installer"
+sleep 3
+add-apt-repository ppa:linuxuprising/java -y
+sleep 3
+
+echo "Update and Upgrade"
+apt update -y && apt upgrade -y
+sleep 3
+
+echo "Bind Java Installer version v17"
+sleep 3
+apt-get install oracle-java17-installer oracle-java17-set-default
+
+sleep 3
+
+echo "Install certificates"
+sleep 3
+apt-get install ca-certificates curl
+sleep 3
+
+echo "Install Docker"
+sleep 3
+curl -ssL https://get.docker.com | sh
+sleep 3
+
+echo "add group Docker"
+sleep 3
+groupadd docker
+sleep 3
+
+echo "add user to group Docker"
+sleep 3
+usermod -aG docker $USER
+
+echo "Docker Installed successfully"
