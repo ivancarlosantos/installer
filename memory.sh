acd82f9b08ee1818b1b26b762cee3b99a429898b
Ivan Santos <ivansantos.ronem@gmail.com>
2025-04-07 12:40:13 -0300

    feat: create memory.sh file

--- memory.sh
+++ memory.sh
@@ -1,21 +1,25 @@
+#!/bin/bash
+
 # executado limpeza do cache, dentries e inodes
 
 echo 'Para limpar o Cache/Buffer de RAM:'
 sleep 2
 sync; echo 1 > /proc/sys/vm/drop_caches
-sleep 2
+sleep 3
 
 echo 'Para limpar Dentries e inodes:'
 sleep 2
 sync; echo 2 > /proc/sys/vm/drop_caches
-sleep 2
+sleep 3
 
 echo 'Para limpar tudo: Cache/Buffer, Dentries e Inodes:'
 sleep 2
 sync; echo 3 > /proc/sys/vm/drop_caches
-sleep 2
+sleep 3
 
 echo 'Para limpar o espaço em disco consumido pela SWAP:'
 sleep 2
 swapoff -a && swapon -a
-sleep 2
+sleep 3
+
+echo 'Processo de clean cache realizado com sucesso\n'
