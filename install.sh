76fcb4a2e4ce94cc85ef2a12f1af52847834f2b4
Ivan Santos <ivansantos.ronem@gmail.com>
2025-04-10 18:01:15 -0300

    feat: metrics agents

--- install.sh
+++ install.sh
@@ -64,6 +64,13 @@ sleep 3
 echo "Download image file Successfully\n"
 sleep 3
 
+echo "Installing the Metrics Agent\n"
+sleep 3
+curl -sSL https://repos.insights.digitalocean.com/install.sh | sh
+sleep 3
+echo "Installing the Metrics Agent Successfully\n"
+sleep 2
+
 echo "Update Machine\n"
 sleep 4
 apt-get update -y
