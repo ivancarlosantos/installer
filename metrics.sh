370e6af62b8c477def20253398c04747f039f975
Ivan Santos <ivansantos.ronem@gmail.com>
2025-07-24 14:46:17 -0300

    Update metrics.sh

--- metrics.sh
+++ metrics.sh
@@ -8,6 +8,18 @@ echo "Criar Rede/Network Docker\n"
 docker network create --driver bridge app-prime-max
 sleep 5
 
+echo "Create installer folder\n"
+sleep 2
+mkdir environment-installer
+sleep 2
+
+echo "Download batchshell file image api-prime-max\n"
+sleep 3
+cd environment-installer && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/prometheus.yml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/promtail-config.yaml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/loki-config.yml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/image.c && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/image.sh && chmod +x image.sh
+sleep 3
+echo "Download image file Successfully\n"
+sleep 3
+
 echo "Instalar Loki"
 docker run --name loki --network app-prime-max -d -v $(pwd):/mnt/config -p 3100:3100 grafana/loki:3.0.0 -config.file=/mnt/config/loki-config.yaml
 sleep 5
