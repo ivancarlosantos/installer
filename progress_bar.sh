4227f145f1bb8c57cd57df4187999eeb85d40f15
Ivan Santos <ivansantos.ronem@gmail.com>
2025-08-30 11:56:10 -0300

    Update progress_bar.sh

--- progress_bar.sh
+++ progress_bar.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# Define o tamanho da barra de progresso
-bar_size=20
-
-# Simula um processo que dura 10 segundos
-for ((i = 0; i <= 100; i++)); do
-  # Calcula o número de blocos preenchidos na barra
-  filled_blocks=$((i * bar_size / 100))
-  
-  # Calcula o número de blocos vazios na barra
-  empty_blocks=$((bar_size - filled_blocks))
-  
-  # Cria a barra de progresso
-  progress_bar=$(echo "%${filled_blocks}s" | tr ' ' '#')
-  progress_bar+=$(echo "%${empty_blocks}s" | tr ' ' '-')
-
-  # Exibe a barra de progresso e a porcentagem
-  echo "\r[${progress_bar}] ${i}%%"
-
-  # Simula um tempo de carregamento
-  sleep 0.1
+# Barra de progresso simples
+for i in {0..100..5}; do
+    # Calcula a barra (20 caracteres de largura)
+    filled=$((i / 5))
+    empty=$((20 - filled))
+    
+    # Constrói a barra
+    bar="["
+    for ((j=0; j<filled; j++)); do
+        bar+="#"
+    done
+    for ((j=0; j<empty; j++)); do
+        bar+=" "
+    done
+    bar+="]"
+    
+    # Exibe
+    printf "\r%s %d%%" "$bar" "$i"
+    sleep 0.1
 done
 
-echo "\nCarregamento concluído!\n"
+echo ""  # Nova linha no final
