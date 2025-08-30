8cf06221cff5e5d3fa4cc2acc5f795611ea7b29b
ivancarlosantos <ivansantos.ronem@gmail.com>
2025-08-30 15:01:39 -0300

    feat: adjust progress bar files

---
+++ progress_deep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Barra de progresso simples
+for i in {0..100..5}; do
+    # Calcula a barra (20 caracteres de largura)
+    filled=$((i / 5))
+    empty=$((20 - filled))
+    
+    # Constrói a barra
+    bar="["
+    for ((j=0; j<filled; j++)); do
+        bar+="#"
+    done
+    for ((j=0; j<empty; j++)); do
+        bar+=" "
+    done
+    bar+="]"
+    
+    # Exibe
+    printf "\r%s %d%%" "$bar" "$i"
+    sleep 0.1
+done
+
+echo ""  # Nova linha no final
