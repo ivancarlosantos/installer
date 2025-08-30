a04f29b52799f149eb783ffa164d8c33fde561a5
Ivan Santos <ivansantos.ronem@gmail.com>
2025-08-30 11:51:36 -0300

    Update and rename progress_bar_1.sh to progress_bar.sh

---
+++ progress_bar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define o tamanho da barra de progresso
+bar_size=20
+
+# Simula um processo que dura 10 segundos
+for ((i = 0; i <= 100; i++)); do
+  # Calcula o número de blocos preenchidos na barra
+  filled_blocks=$((i * bar_size / 100))
+  
+  # Calcula o número de blocos vazios na barra
+  empty_blocks=$((bar_size - filled_blocks))
+  
+  # Cria a barra de progresso
+  progress_bar=$(printf "%${filled_blocks}s" | tr ' ' '#')
+  progress_bar+=$(printf "%${empty_blocks}s" | tr ' ' '-')
+
+  # Exibe a barra de progresso e a porcentagem
+  printf "\r[${progress_bar}] ${i}%%"
+
+  # Simula um tempo de carregamento
+  sleep 0.1
+done
+
+printf "\nCarregamento concluído!\n"
