5d734a68b08d91e83f0d2898b18f9f1b85f3af8a
Ivan Santos <ivansantos.ronem@gmail.com>
2025-08-30 11:35:07 -0300

    Create progress_bar_1.sh

---
+++ progress_bar_1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Função para desenhar a barra de progresso
+progress_bar() {
+    local current=$1
+    local total=$2
+    local width=${3:-50}  # Largura da barra (padrão: 50 caracteres)
+    
+    # Calcula a porcentagem
+    local percent=$((current * 100 / total))
+    
+    # Calcula quantos caracteres preencher
+    local filled=$((width * current / total))
+    
+    # Cria a barra
+    local bar=""
+    for ((i=0; i<width; i++)); do
+        if [ $i -lt $filled ]; then
+            bar="${bar}█"
+        else
+            bar="${bar}░"
+        fi
+    done
+    
+    # Imprime a barra com porcentagem
+    printf "\r[%s] %3d%%" "$bar" "$percent"
+    
+    # Se chegou a 100%, adiciona nova linha
+    if [ $current -eq $total ]; then
+        echo
+    fi
+}
