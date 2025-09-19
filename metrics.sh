5e8578b35dfc2be0b585170ecc5965c9f7f7c99e
ivancarlosantos <ivansantos.ronem@gmail.com>
2025-09-19 14:52:38 -0300

    fix: ports containers

--- metrics.sh
+++ metrics.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo "OS Release!\n"
+echo "OS Release!"
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/release.sh | bash
 sleep 5
 
-echo "Criar Rede/Network Docker\n"
+echo "Criar Rede/Network Docker"
 docker network create --driver bridge app-video-max
 sleep 5
 
-echo "Create installer folder\n"
+echo "Create installer folder"
 sleep 2
 mkdir environment-installer
 sleep 2
 
-echo "Download batchshell file image api-video-max\n"
+echo "Download batchshell file image api-video-max"
 sleep 3
 cd environment-installer && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/prometheus.yml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/promtail-config.yaml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/loki-config.yml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/image.c && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/image.sh && chmod +x image.sh
 sleep 3
-echo "Download image file Successfully\n"
+echo "Download image file Successfully"
 sleep 3
 
 echo "Portainer"
