766b579962467a31b8ad10f02120047e992d5cf5
ivancarlosantos <ivansantos.ronem@gmail.com>
2025-09-19 14:21:08 -0300

    feat: add image/containers

--- metrics.sh
+++ metrics.sh
@@ -5,7 +5,7 @@ curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/head
 sleep 5
 
 echo "Criar Rede/Network Docker\n"
-docker network create --driver bridge app-prime-max
+docker network create --driver bridge app-video-max
 sleep 5
 
 echo "Create installer folder\n"
@@ -13,23 +13,27 @@ sleep 2
 mkdir environment-installer
 sleep 2
 
-echo "Download batchshell file image api-prime-max\n"
+echo "Download batchshell file image api-video-max\n"
 sleep 3
 cd environment-installer && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/prometheus.yml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/promtail-config.yaml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/loki-config.yml && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/image.c && curl -O https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/image.sh && chmod +x image.sh
 sleep 3
 echo "Download image file Successfully\n"
 sleep 3
 
+echo "Portainer"
+docker run -d -p 9000:9000 --name portainer --network app-video-max --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce:lts
+sleep 5
+
 echo "Instalar Loki"
-docker run --name loki --network app-prime-max -d -v $(pwd):/mnt/config -p 3100:3100 grafana/loki:3.0.0 -config.file=/mnt/config/loki-config.yaml
+docker run --name loki --network app-video-max -d -v $(pwd):/mnt/config -p 3100:3100 grafana/loki:3.0.0 -config.file=/mnt/config/loki-config.yaml
 sleep 5
 
 echo "Instalar Promtail"
-docker run --name promtail --network app-prime-max -d -v $(pwd):/mnt/config -v /var/log:/var/log --link loki grafana/promtail:3.0.0 -config.file=/mnt/config/promtail-config.yaml
+docker run --name promtail --network app-video-max -d -v $(pwd):/mnt/config -v /var/log:/var/log --link loki grafana/promtail:3.0.0 -config.file=/mnt/config/promtail-config.yaml
 sleep 5
 
 echo "Instalar Grafana"
-docker run --name grafana --network app-prime-max -d -p 3001:3000 --link loki grafana/grafana
+docker run --name grafana --network app-video-max -d -p 7000:3000 --link loki grafana/grafana
 sleep 5
 
 echo "Instalar Plugin Grafana-Loki"
@@ -37,9 +41,33 @@ docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all
 sleep 5
 
 echo "Instalar Prometheus"
-docker run --name prometheus --network app-prime-max -d -p 9090:9090 -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v3.2.1
+docker run --name prometheus --network app-video-max -d -p 9090:9090 -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v3.2.1
 sleep 5
 
 echo "Instalar Cadvisor"
-docker run --name cadvisor --network app-prime-max -d -p 8083:8080 -v /:/rootfs:ro -v /var/run:/var/run:ro -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro -v /dev/disk/:/dev/disk:ro gcr.io/cadvisor/cadvisor:latest
+docker run --name cadvisor --network app-video-max -d -p 8083:8080 -v /:/rootfs:ro -v /var/run:/var/run:ro -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro -v /dev/disk/:/dev/disk:ro gcr.io/cadvisor/cadvisor:latest
+sleep 5
+
+echo "Instalar Uptime Kuma"
+docker run --name uptime-kuma --restart=always -d -p 8082:3001 -v uptime-kuma:/app/data -v /var/run/docker.sock:/var/run/docker.sock louislam/uptime-kuma:1
+sleep 5
+
+echo "Instalar RabbitMQ"
+docker run --name rabbitmq --hostname rabbit-host --network app-video-max -d -p 5672:5672 -p 8084:15672 rabbitmq:4.0-management-alpine
+sleep 5
+
+echo "Instalar Postgres"
+docker run --name postgres --network app-video-max -d -p 5432:5432 -e POSTGRES_PASSWORD='12345' -e POSTGRES_USER='postgres' -e POSTGRES_DB='video-max' postgres:15
+sleep 5
+
+echo "PGAdmin"
+docker run --name pgadmin --network app-video-max -d -p 15432:80 -e PGADMIN_DEFAULT_EMAIL='video-max@mail.com.br' -e PGADMIN_DEFAULT_PASSWORD='video-max@mail.com.br' dpage/pgadmin4:latest
+sleep 5
+
+echo "Video Max"
+docker run --name video-max -d -p 8080:8080 -e DB_URL='jdbc:postgresql://postgres:5432/video-max' -e DB_USERNAME='postgres' -e DB_PASSWORD='12345' -e RABBITMQ_HOST='rabbit-host' devmenorzera/video-max:latest
+sleep 5
+
+echo "Update User Plan"
+docker run --name user-plan -d -p 8081:8081 -e DB_URL='jdbc:postgresql://postgres:5432/video-max' -e DB_USERNAME='postgres' -e DB_PASSWORD='12345' -e RABBITMQ_HOST='rabbit-host' devmenorzera/user-plan:latest
 sleep 5
