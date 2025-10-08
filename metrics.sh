8af299a135b98728230cf94ac7a1f231b30147b0
ivancarlosantos <ivansantos.ronem@gmail.com>
2025-10-08 18:42:50 -0300

    feat: add environment variables minio storage s3

--- metrics.sh
+++ metrics.sh
@@ -85,7 +85,7 @@ sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Video Max"
-docker run --name video-max --network app-video-max -d -p 8080:8080 -e DB_URL='jdbc:postgresql://postgres:5432/video-max' -e DB_USERNAME='postgres' -e DB_PASSWORD='12345' -e RABBITMQ_HOST='rabbit-host' devmenorzera/video-max:latest
+docker run --name video-max --network app-video-max -d -p 8080:8080 -e DB_URL='jdbc:postgresql://postgres:5432/video-max' -e DB_USERNAME='postgres' -e DB_PASSWORD='12345' -e RABBITMQ_HOST='rabbit-host' -e ACCESS_KEY='admin' -e BUCKET_NAME='user-images' -e SECRET_KEY='icarlos@icarlos' devmenorzera/video-max:latest
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
