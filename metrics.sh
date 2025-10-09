527e5c2569057f365ddbd19680e401208e16ffd8
ivancarlosantos <ivansantos.ronem@gmail.com>
2025-10-08 23:33:51 -0300

    fix: network

--- metrics.sh
+++ metrics.sh
@@ -25,17 +25,17 @@ echo "Download Files Successfully"
 sleep 3
 
 echo "Portainer"
-docker run -d -p 9000:9000 --name portainer --network app-video-max --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce:lts
+docker run --name='portainer' --network='app-video-max' -d -p 9001:9000 --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce:lts
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Instalar Promtail"
-docker run --name promtail --network app-video-max -d -v $(pwd):/mnt/config -v /var/log:/var/log --link loki grafana/promtail:3.0.0 -config.file=/mnt/config/promtail-config.yaml
+docker run --name='promtail' --network='app-video-max' -d -v $(pwd):/mnt/config -v /var/log:/var/log --link loki grafana/promtail:3.0.0 -config.file=/mnt/config/promtail-config.yaml
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Instalar Grafana"
-docker run --name grafana --network app-video-max -d -p 7000:3000 --link loki grafana/grafana
+docker run --name='grafana' --network='app-video-max' -d -p 3000:3000 --link loki grafana/grafana
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
@@ -45,27 +45,27 @@ sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Instalar Prometheus"
-docker run --name prometheus --network app-video-max -d -p 9090:9090 -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v3.2.1
+docker run --name='prometheus' --network='app-video-max' -d -p 9090:9090 -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v3.2.1
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Instalar cAdvisor"
-docker run --name cadvisor --network app-video-max -d -p 8083:8080 -v /:/rootfs:ro -v /var/run:/var/run:ro -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro -v /dev/disk/:/dev/disk:ro gcr.io/cadvisor/cadvisor:latest
+docker run --name='cadvisor' --network='app-video-max' -d -p 8083:8080 -v /:/rootfs:ro -v /var/run:/var/run:ro -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro -v /dev/disk/:/dev/disk:ro gcr.io/cadvisor/cadvisor:latest
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Instalar Uptime Kuma"
-docker run --name uptime-kuma --network app-video-max --restart=always -d -p 3001:3001 -v uptime-kuma:/app/data -v /var/run/docker.sock:/var/run/docker.sock louislam/uptime-kuma:1
+docker run --name='uptime-kuma' --network='app-video-max' --restart=always -d -p 3001:3001 -v uptime-kuma:/app/data -v /var/run/docker.sock:/var/run/docker.sock louislam/uptime-kuma:1
 sleep 3
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Instalar RabbitMQ"
-docker run --name rabbitmq --hostname rabbit-host --network app-video-max -d -p 5672:5672 -p 15672:15672 rabbitmq:4.0-management-alpine
+docker run --name='rabbitmq' --network='app-video-max' --hostname='rabbit-host' -d -p 5672:5672 -p 15672:15672 rabbitmq:4.0-management-alpine
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Instalar Minio"
-docker run --name minio --network app-video-max -d -p 9002:9000 -p 9001:9001 -e MINIO_ROOT_USER='admin' -e MINIO_ROOT_PASSWORD='icarlos@icarlos' minio/minio:latest server /data --console-address ":9001"
+docker run --name='minio' --network='app-video-max' -d -p 9000:9000 -p 9001:9001 -e MINIO_ROOT_USER='admin' -e MINIO_ROOT_PASSWORD='icarlos@icarlos' minio/minio:latest server /data --console-address ":9001"
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
@@ -75,12 +75,12 @@ sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "PGAdmin SGBD"
-docker run --name pgadmin --network app-video-max -d -p 15432:80 -e PGADMIN_DEFAULT_EMAIL='video-max@mail.com.br' -e PGADMIN_DEFAULT_PASSWORD='video-max@mail.com.br' dpage/pgadmin4:latest
+docker run --name pgadmin --network app-video-max -d -p 15432:80 -e PGADMIN_DEFAULT_EMAIL='admin' -e PGADMIN_DEFAULT_PASSWORD='icarlos@icarlos' dpage/pgadmin4:latest
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
 echo "Video Max"
-docker run --name video-max --network app-video-max -d -p 8080:8080 -e DB_URL='jdbc:postgresql://postgres:5432/video-max' -e DB_USERNAME='postgres' -e DB_PASSWORD='12345' -e RABBITMQ_HOST='rabbit-host' -e ACCESS_KEY='admin' -e BUCKET_NAME='user-images' -e SECRET_KEY='icarlos@icarlos' devmenorzera/video-max:latest
+docker run --name='video-max' --network='app-video-max' -d -p 8080:8080 -e DB_URL='jdbc:postgresql://postgres:5432/video-max' -e DB_USERNAME='postgres' -e DB_PASSWORD='12345' -e RABBITMQ_HOST='rabbit-host' -e ACCESS_KEY='admin' -e BUCKET_NAME='user-images' -e SECRET_KEY='icarlos@icarlos' devmenorzera/video-max:latest
 sleep 5
 curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
 
