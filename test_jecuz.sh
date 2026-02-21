0cae6cf06353e50c1355fa2ed1667d6bfa2902de
Ivan Santos <ivansantos.ronem@gmail.com>
2026-02-21 08:52:23 -0300

    Refactor test_jecuz.sh to streamline health checks
    Removed redundant actuator health checks and added application URL test.

--- test_jecuz.sh
+++ test_jecuz.sh
@@ -3,29 +3,14 @@
 echo "SmokeTest Jecuz URL"
 sleep 3
 
-echo "Required actuator"
-sleep 3
-curl http://localhost:8080/actuator 
-sleep 2
-
 echo "Required health"
 sleep 3
 curl http://localhost:8080/actuator/health 
 sleep 2
 
-echo "Required health"
+echo "Application URL Test"
 sleep 3
-curl http://localhost:8080/actuator/health 
-sleep 2
-
-echo "Required health"
-sleep 3
-curl http://localhost:8080/actuator/health 
-sleep 2
-
-echo "Required health"
-sleep 3
-curl http://localhost:8080/actuator/health 
+curl http://localhost:8080/test 
 sleep 2
 
 echo "SmokeTest Successfully!!"
