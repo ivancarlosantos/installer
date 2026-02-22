dbf80c6ceb5de6571daea5e67135119e84ef7d84
Ivan Santos <ivansantos.ronem@gmail.com>
2026-02-22 11:48:56 -0300

    Refactor API test script to remove call_api function

--- test_jecuz.sh
+++ test_jecuz.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
 
-BASE_URL=http://localhost:8080
-
 echo "=============================================="
 echo "🚀 INICIANDO TESTES AUTOMATIZADOS DA API"
 echo "=============================================="
-
-function call_api() {
-  DESCRIPTION=$1
-  REQUEST=$2
-
-  echo ""
-  echo "➡️  $DESCRIPTION"
-  echo "----------------------------------------------"
-
-  RESPONSE=$(eval $REQUEST -w "\n%{http_code}")
-  HTTP_BODY=$(echo "$RESPONSE" | sed '$d')
-  HTTP_STATUS=$(echo "$RESPONSE" | tail -n1)
-
-  echo "Status: $HTTP_STATUS"
-  echo "Response:"
-  echo "$HTTP_BODY"
-
-  if [[ "$HTTP_STATUS" -ge 200 && "$HTTP_STATUS" -lt 300 ]]; then
-    echo "✅ SUCESSO"
-  else
-    echo "❌ ERRO - Abortando execução"
-    exit 1
-  fi
-}
 
 # ---------------------------------------------------
 # 1️⃣ Started Docker Container Database
@@ -49,37 +23,37 @@ sleep 20
 # ---------------------------------------------------
 # 1️⃣ Health Check
 # ---------------------------------------------------
-call_api "Health Check"
+echo "Health Check"
 curl GET http://localhost:8080/actuator/health
 
 # ---------------------------------------------------
 # 2️⃣ Endpoint Teste
 # ---------------------------------------------------
-call_api "Endpoint Teste"
+echo "Endpoint Teste"
 curl GET http://localhost:8080/test
 
 # ---------------------------------------------------
 # 3️⃣ Criar Cliente
 # ---------------------------------------------------
-call_api "Criar Cliente"
+echo "Criar Cliente"
 curl -X POST http://localhost:8080/api/cliente/save?nome=NameFullName&nascimento=22/02/1990&telefone=%2B244854587887&numeroBi=001668196RS032&email=mail@mail.com
 
 # ---------------------------------------------------
 # 4️⃣ Criar Diarista
 # ---------------------------------------------------
-call_api "Criar Diarista"
+echo "Criar Diarista"
 curl -X POST http://localhost:8080/api/diarista/save?nome=NameFullName&nascimento=22/02/1990&telefone=%2B244854587878&numeroBi=001668196TQ032&email=mail@mail.com
 
 # ---------------------------------------------------
 # 5️⃣ Gerar Ordem de Serviço
 # ---------------------------------------------------
-call_api "Gerar Ordem de Serviço"
+echo "Gerar Ordem de Serviço"
 curl -X POST http://localhost:8080/api/ordem/servico/gerar?idCliente=1&idDiarista=1&tipoLimpeza=RESIDENCIAL&dataSolicitacao=15/02/2026&descricaoTarefa=limpeza%20comodos&valor=200.0&dataExecucao=19/02/2026
 
 # ---------------------------------------------------
 # 6️⃣ Listar Ordens
 # ---------------------------------------------------
-call_api "Listar Ordens de Serviço"
+echo "Listar Ordens de Serviço"
 curl GET http://localhost:8080/api/ordem/servico/list?search=&page=0&size=10
 
 # ---------------------------------------------------
