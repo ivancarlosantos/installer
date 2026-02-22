8438bb64f12689a5a373c19af284a5ee24b13552
Ivan Santos <ivansantos.ronem@gmail.com>
2026-02-22 11:33:14 -0300

    Refactor comments and streamline API calls

--- test_jecuz.sh
+++ test_jecuz.sh
@@ -39,7 +39,7 @@ docker run --name='jecuz_db' --network='jecuz_app' -d -p 5432:5432 -e POSTGRES_P
 sleep 20
 
 # ---------------------------------------------------
-# 1️⃣ Initializr Docker Container
+# 1️⃣ Initializr Docker Application API
 # ---------------------------------------------------
 echo 'initialzr docker container'
 sleep 3
@@ -49,38 +49,38 @@ sleep 20
 # ---------------------------------------------------
 # 1️⃣ Health Check
 # ---------------------------------------------------
-call_api "Health Check" \
+call_api "Health Check"
 curl GET $BASE_URL/actuator/health
 
 # ---------------------------------------------------
 # 2️⃣ Endpoint Teste
 # ---------------------------------------------------
-call_api "Endpoint Teste" \
+call_api "Endpoint Teste"
 curl GET $BASE_URL/test
 
 # ---------------------------------------------------
 # 3️⃣ Criar Cliente
 # ---------------------------------------------------
-call_api "Criar Cliente" \
-curl -X POST \"$BASE_URL/api/cliente/save?nome=NameFullName&nascimento=22/02/1990&telefone=%2B244854587887&numeroBi=001668196RS032&email=mail@mail.com
+call_api "Criar Cliente"
+curl -X POST $BASE_URL/api/cliente/save?nome=NameFullName&nascimento=22/02/1990&telefone=%2B244854587887&numeroBi=001668196RS032&email=mail@mail.com
 
 # ---------------------------------------------------
 # 4️⃣ Criar Diarista
 # ---------------------------------------------------
-call_api "Criar Diarista" \
-curl -X POST \"$BASE_URL/api/diarista/save?nome=NameFullName&nascimento=22/02/1990&telefone=%2B244854587878&numeroBi=001668196TQ032&email=mail@mail.com
+call_api "Criar Diarista"
+curl -X POST $BASE_URL/api/diarista/save?nome=NameFullName&nascimento=22/02/1990&telefone=%2B244854587878&numeroBi=001668196TQ032&email=mail@mail.com
 
 # ---------------------------------------------------
 # 5️⃣ Gerar Ordem de Serviço
 # ---------------------------------------------------
-call_api "Gerar Ordem de Serviço" \
-curl -X POST \"$BASE_URL/api/ordem/servico/gerar?idCliente=1&idDiarista=1&tipoLimpeza=RESIDENCIAL&dataSolicitacao=15/02/2026&descricaoTarefa=limpeza%20comodos&valor=200.0&dataExecucao=19/02/2026
+call_api "Gerar Ordem de Serviço"
+curl -X POST $BASE_URL/api/ordem/servico/gerar?idCliente=1&idDiarista=1&tipoLimpeza=RESIDENCIAL&dataSolicitacao=15/02/2026&descricaoTarefa=limpeza%20comodos&valor=200.0&dataExecucao=19/02/2026
 
 # ---------------------------------------------------
 # 6️⃣ Listar Ordens
 # ---------------------------------------------------
-call_api "Listar Ordens de Serviço" \
-curl GET \"$BASE_URL/api/ordem/servico/list?search=&page=0&size=10
+call_api "Listar Ordens de Serviço"
+curl GET $BASE_URL/api/ordem/servico/list?search=&page=0&size=10
 
 echo ""
 echo "=============================================="
