1ffc1d694a7876bfc1a753fa6f1066cd7e12f3da
ivancarlosantos <ivansantos.ronem@gmail.com>
2026-07-26 17:20:37 -0300

    feat: script de instalação para aula de banco de dados da comunidade

---
+++ database_aula.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "=============================================="
+echo "🚀 INICIANDO DOWNLOAD DA BASE DE DADOS"
+echo "=============================================="
+
+sleep 3
+curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
+
+sleep 2
+
+echo "===================================================="
+echo "🚀 CONSTRUINDO REDE PARA CONEXÃO DOS CONTAINERS"
+echo "===================================================="
+
+sleep 3
+echo ' '
+docker network create --driver bridge aula_net
+
+sleep 3
+
+echo "==========================================================="
+echo "🚀 BAIXANDO AS IMAGENS DO BANCO POSTGRES E SGBG PGADMIN"
+echo "==========================================================="
+
+sleep 2
+
+echo ' '
+docker run --name='database-aula' --network='aula_net' -d -p 5433:5432 -e POSTGRES_PASSWORD='aula' -e POSTGRES_USER='aula' -e POSTGRES_DB='database_aula' postgres:15
+sleep 20
+
+curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
+
+docker run --name='pgadmin' --network='aula_net' -d -p 15432:80 -e PGADMIN_DEFAULT_EMAIL='aula@aula.com' -e PGADMIN_DEFAULT_PASSWORD='aula@aula.com' dpage/pgadmin4:latest
+sleep 5
+
+curl -fsSL https://raw.githubusercontent.com/ivancarlosantos/installer/refs/heads/master/progress_bar_spinner.sh | bash
+echo ' '
+echo "================================================="
+echo "🎉 TODOS OS CONTAINERS EXECUTADOS COM SUCESSO"
+echo "================================================="
